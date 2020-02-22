3429fe40f5858b1c5cb7cbd80f1d65e65a87b38d
Semyon Kondratev <destruction.industry@gmail.com>
2020-02-22 16:54:15 +0300

    Made a premake

---
+++ premake5.lua
@@ -0,0 +1,112 @@
+workspace "exEngine"
+	architecture "x64"
+
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+
+project "exEngine"
+	location "exEngine"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files 
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs 
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"EX_PLATFORM_WINDOWS",
+			"EX_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "EX_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "EX_RELEASE"
+		optimize "On"
+
+
+	filter "configurations:Dist"
+		defines "EX_DIST"
+		optimize "On"
+
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files 
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	links 
+	{
+		"exEngine"
+	}
+
+
+	includedirs 
+	{
+		"exEngine/vendor/spdlog/include",
+		"exEngine/src"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"EX_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "EX_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "EX_RELEASE"
+		optimize "On"
+
+
+	filter "configurations:Dist"
+		defines "EX_DIST"
+		optimize "On"
